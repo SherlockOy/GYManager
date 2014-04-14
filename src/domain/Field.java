5a27796301a964d35d8e7c7c0807f63ba0325176
SherlockOy <oyang716@qq.com>
2014-04-14 12:24:00 +0800

    initial commit

---
+++ src/domain/Field.java
@@ -0,0 +1,31 @@
+package domain;
+
+public class Field {
+	private String fieldId;
+	private String item;
+	private String timeSection;
+
+	public String getFieldId() {
+		return fieldId;
+	}
+
+	public void setFieldId(String fieldId) {
+		this.fieldId = fieldId;
+	}
+
+	public String getItem() {
+		return item;
+	}
+
+	public void setItem(String item) {
+		this.item = item;
+	}
+
+	public String getTimeSection() {
+		return timeSection;
+	}
+
+	public void setTimeSection(String timeSection) {
+		this.timeSection = timeSection;
+	}
+}
