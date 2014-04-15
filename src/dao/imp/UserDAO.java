d4fb9b55d000f8a0cf4e8a61c06efcd1f6922390
SherlockOy <oyang716@qq.com>
2014-04-15 13:36:16 +0800

    add gitignore

---
+++ src/dao/imp/UserDAO.java
@@ -0,0 +1,58 @@
+package dao.imp;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+
+import dao.IUserDAO;
+import domain.User;
+
+public class UserDAO implements IUserDAO {
+
+	protected Session getSession() {
+		Configuration configuration = new Configuration();
+		configuration.configure();
+		ServiceRegistry serviceRegistry = new StandardServiceRegistryBuilder()
+				.applySettings(configuration.getProperties()).build();
+		SessionFactory sessionFactory = configuration
+				.buildSessionFactory(serviceRegistry);
+		Session session = sessionFactory.openSession();
+		Transaction transaction = session.beginTransaction();
+		return session;
+	}
+
+	@Override
+	public void addUser(User user) {
+		// TODO Auto-generated method stub
+		this.getSession().save(user);
+
+	}
+
+	@Override
+	public void updateUser(User user) {
+		// TODO Auto-generated method stub
+		this.getSession().update(user);
+	}
+
+	@Override
+	public void deleteUser(User user) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public User getUserById(String userId) {
+		// TODO Auto-generated method stub
+		return (User) this.getSession().get(User.class, userId);
+	}
+
+	@Override
+	public User getUserByUserName(String userName) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
