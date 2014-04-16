94ff5fa843a259a4b49ae21bca7691ff3859c563
SherlockOy <oyang716@qq.com>
2014-04-16 21:17:18 +0800

    complete the test of the Hibernate and finish the UserDAO

--- src/dao/imp/UserDAO.java
+++ src/dao/imp/UserDAO.java
@@ -17,43 +17,80 @@ public class UserDAO implements IUserDAO {
 	public void addUser(User user) {
 		// TODO Auto-generated method stub
 		Session session = HibernateUtil.getSession();
-		session.save(user);
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.save(user);
+			transaction.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			transaction.rollback();
+		}
+		HibernateUtil.closeSession();
 	}
 
 	@Override
 	public void updateUser(User user) {
 		// TODO Auto-generated method stub
 		Session session = HibernateUtil.getSession();
-		session.update(user);
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.update(user);
+			transaction.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			transaction.rollback();
+		}
+		HibernateUtil.closeSession();
+
 	}
 
 	@Override
 	public void deleteUser(User user) {
 		// TODO Auto-generated method stub
 		Session session = HibernateUtil.getSession();
-		session.delete(user);
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			session.delete(user);
+			transaction.commit();
+		} catch (Exception ex) {
+			ex.printStackTrace();
+			transaction.rollback();
+		}
+		HibernateUtil.closeSession();
 	}
 
 	@Override
 	public User getUserById(String userId) {
 		// TODO Auto-generated method stub
-		return (User) HibernateUtil.getSession().get(User.class, userId);
+		Session session = HibernateUtil.getSession();
+		User user = (User)session.get(User.class, userId);
+		HibernateUtil.closeSession();
+		return user;
 	}
 
 	@Override
 	public User getUserByUserName(String userName) {
 		// TODO Auto-generated method stub
-		return (User) HibernateUtil.getSession().get(User.class, userName);
+		Session session = HibernateUtil.getSession();
+		User user = (User)session.get(User.class, userName);
+		HibernateUtil.closeSession();
+		return user;
 	}
 
-	public static void main(String[] args){
-		
+	/*
+	 * for the test
+	 */
+	/*public static void main(String[] args) {
+
 		User user = new User();
 		user.setUserName("userName");
 		user.setPassWord("123");
-		
+
 		UserDAO userDAO = new UserDAO();
 		userDAO.addUser(user);
 	}
-	
+	*/
 }
