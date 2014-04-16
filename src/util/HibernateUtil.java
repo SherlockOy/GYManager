cfe07e3bf1749794f9f0652a81c8fbf4aea429b0
SherlockOy <oyang716@qq.com>
2014-04-16 16:26:28 +0800

    compelet HibernateUtil.java

--- src/util/HibernateUtil.java
+++ src/util/HibernateUtil.java
@@ -1,12 +1,15 @@
 package util;
 
+import org.hibernate.HibernateException;
+import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 public class HibernateUtil {
-	private static final SessionFactory sessionFactory;
+	private static SessionFactory sessionFactory;
+	private static final ThreadLocal<Session> threadLocal = new ThreadLocal<Session>();
 
 	static {
 
@@ -24,9 +27,44 @@ public class HibernateUtil {
 
 	}
 
-	public static SessionFactory getSessionFactory() {
-
-		return sessionFactory;
+	private HibernateUtil() {
 	}
 
+	public static SessionFactory getSessionFactory() {
+		return sessionFactory;
+	}
+	//用来获取会话
+	public static Session getSession() throws HibernateException {
+		Session session = (Session) threadLocal.get();
+		if (session == null || !session.isOpen()) {
+			if (sessionFactory == null) {
+				rebuildSessionFactory();
+			}
+			session = (sessionFactory != null) ? sessionFactory.openSession()
+					: null;
+			threadLocal.set(session);
+		}
+		return session;
+	}
+	//用来重新建立SessionFactory对象
+	public static void rebuildSessionFactory() {
+		try {
+			Configuration cfg = new Configuration()
+					.configure("hibernate.cfg.xml");
+			StandardServiceRegistryBuilder sb = new StandardServiceRegistryBuilder();
+			sb.applySettings(cfg.getProperties());
+			StandardServiceRegistry standardServiceRegistry = sb.build();
+			sessionFactory = cfg.buildSessionFactory(standardServiceRegistry);
+		} catch (Throwable th) {
+			System.err.println("Enitial SessionFactory creation failed" + th);
+			throw new ExceptionInInitializerError(th);
+		}
+	}
+	//用来关闭会话
+	public static void closeSession() throws HibernateException {
+		Session session = (Session) threadLocal.get();
+		threadLocal.set(null);
+		if (session != null)
+			session.close();
+	}
 }
