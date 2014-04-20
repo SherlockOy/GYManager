ab90fc1199d5512d8b52a155d3942796944a7870
SherlockOy <oyang716@qq.com>
2014-04-20 21:23:01 +0800

    add RegesterAction.java

---
+++ src/action/RegisterAction.java
@@ -0,0 +1,13 @@
+package action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class RegisterAction extends ActionSupport {
+	
+	private String userId;
+	private String userName;
+	private String passWord;
+	private String email;
+	
+
+}
